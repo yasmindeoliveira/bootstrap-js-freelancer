7e58b36779549dbc102eed902b2fa08cee26d8d2
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-18 15:49:31 +0100

    completato calcolo lavoro

---
+++ script.js
@@ -0,0 +1,54 @@
+//fate in modo che quando l’utente fa click sul bottone “send” del form, il sito vi calcoli l’ammontare del vostro lavoro per le ore di lavoro richieste dall’utente.
+
+//Se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.50 € l’ora
+//Se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.30 € l’ora
+//Se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.60 € l'ora
+
+/* 
+Se poi l’utente inserisce un codice promozionale tra i seguenti YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24, fate in modo che l’utente abbia diritto ad uno sconto del 25% sul prezzo finale.
+Se il codice inserito non è valido, informate l’utente che il codice è sbagliato e calcolate il prezzo finale senza applicare sconti.
+Mostrare il risultato del calcolo del prezzo finale in una “forma umana” in un apposito tag HTML appena sotto il bottone send.
+*/
+
+function submitForm(event){
+    event.preventDefault();
+
+    calcoloLavoro();
+}
+
+function calcoloLavoro (){
+
+    let work = document.getElementById("work").value;
+    work = parseInt(work);
+    switch (work){
+        case 1:
+            work = 20.5;
+            break;
+
+        case 2:
+            work = 15.3;
+            break;
+
+        case 3:
+            work = 33.6;
+            break;
+
+        default:
+            console.log("Pedra ha sempre RAGIONE!!!!");
+    };
+
+    console.log(work);
+    
+}
+
+
+function calcoloPrezzi (ore, lavoro){
+
+    prezzo = ore*lavoro;
+    return prezzo.tofixed(2)
+}
+
+function sconto (){
+
+    
+}
